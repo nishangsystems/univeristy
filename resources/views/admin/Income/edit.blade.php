51e6cb8cfd712036bac0a660057bf8bb3fb7f422
Suh Edmond <suhedmond25@yahoo.com>
2021-06-18 21:57:48 +0100

    refactor database and views income and expenses

--- resources/views/admin/Income/edit.blade.php
+++ resources/views/admin/Income/edit.blade.php
@@ -4,7 +4,7 @@
 <div class="mx-3">
     <div class="form-panel">
         <form class="form-horizontal" role="form" method="POST" action="{{route('admin.income.update', $income->id)}}">
-            <h5 class="mt-5 font-weight-bold mb-5">Update School Income</h5>
+
             @csrf
             @include('admin.Income.update_form')
             @method('PUT')
--- resources/views/admin/Income/form.blade.php
+++ resources/views/admin/Income/form.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group @error('name') has-error @enderror ">
-    <label for="cname" class="control-label col-lg-2">Name *</label>
+    <label for="cname" class="control-label col-lg-2">Name <span style="color:red">*</span></label>
     <div class="col-lg-10">
         <input class=" form-control" name="name" value="{{old('name')}}" type="text" required />
         @error('name')
@@ -8,7 +8,7 @@
     </div>
 </div>
 <div class="form-group @error('amount') has-error @enderror">
-    <label for="cname" class="control-label col-lg-2">Amount *</label>
+    <label for="cname" class="control-label col-lg-2">Amount <span style="color:red">*</span></label>
     <div class="col-lg-10">
         <input class=" form-control" name="amount" value="{{old('amount')}}" type="number" required />
         @error('amount')
@@ -17,7 +17,7 @@
     </div>
 </div>
 <div class="form-group @error('income_date') has-error @enderror">
-    <label for="cname" class="control-label col-lg-2">Income date *</label>
+    <label for="cname" class="control-label col-lg-2">Income date <span style="color:red">*</span></label>
     <div class="col-lg-10">
         <input class=" form-control" name="income_date" value="{{old('income_date')}}" type="date" required />
         @error('income_date')
@@ -26,7 +26,7 @@
     </div>
 </div>
 <div class="form-group @error('description') has-error @enderror">
-    <label for="cname" class="control-label col-lg-2">Description *</label>
+    <label for="cname" class="control-label col-lg-2">Description <span style="color:red">*</span></label>
     <div class="col-lg-10">
         <textarea class=" form-control" name="description" value="{{old('description')}}" type="textarea" rows="5"></textarea>
         @error('description')
