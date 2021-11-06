844f9c89c6af0767f0065f6d2e67ecb78500db2d
Suh-Edmond <suhedmond25@yahoo.com>
2021-11-06 18:23:06 +0100

    resolve bug with permissions and role seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,11 @@ use Database\Seeders\SchoolUnitSeeder;
 use Database\Seeders\StudentClassSeeder;
 use Database\Seeders\StudentSeeder;
 use Database\Seeders\UserSeeder;
+use Database\Seeders\PermissionSeeder;
+use Database\Seeders\RolesSeeder;
+use Database\Seeders\RolesPermissionSeeder;
+use Database\Seeders\UserRoleSeeder;
+use Database\Seeders\UserPermissionSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,20 +22,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(AdminSeeder::class);
-        $this->call(UnitSeeder::class);
-        $this->call(SubjectSeeder::class);
-        $this->call(ConfigSeeder::class);
-        $this->call(SchoolUnitSeeder::class);
-        $this->call(UserSeeder::class);
-        $this->call(StudentSeeder::class);
-        $this->call(StudentClassSeeder::class);
-        $this->call(ClassSubjectSeeder::class);
-        $this->call(BatchSeeder::class);
-        $this->call(PermissionSeeder::class);
-        $this->call(RolesSeeder::class);
-        $this->call(RolesPermissionSeeder::class);
-        $this->call(UserRole::class);
+        // $this->call(AdminSeeder::class);
+        // $this->call(UnitSeeder::class);
+        // $this->call(SubjectSeeder::class);
+        // $this->call(ConfigSeeder::class);
+        // $this->call(SchoolUnitSeeder::class);
+        // $this->call(UserSeeder::class);
+        // $this->call(StudentSeeder::class);
+        // $this->call(StudentClassSeeder::class);
+        // $this->call(ClassSubjectSeeder::class);
+        // $this->call(BatchSeeder::class);
+       // $this->call(PermissionSeeder::class);
+        //  $this->call(RolesSeeder::class);
+       // $this->call(RolesPermissionSeeder::class);
+       //$this->call(UserRoleSeeder::class);
         $this->call(UserPermissionSeeder::class);
     }
 }
