006949c2071939a9b744939920859d57bfb24e4f
Suh-Edmond <suhedmond25@yahoo.com>
2021-11-06 19:57:08 +0100

    add routes for boarding_fee installments

--- routes/web.php
+++ routes/web.php
@@ -137,8 +137,10 @@ Route::prefix('admin')->name('admin.')->middleware('isAdmin')->group(function ()
 
     Route::get('boarding_fee/create', 'Admin\BoardingFeeController@create')->name('boarding_fee');
     Route::post('boarding_fee', 'Admin\BoardingFeeController@store')->name('boarding_fee.store');
+    Route::post('boarding_fee', 'Admin\BoardingFeeController@addInstallments')->name('boarding_fee.installments.store');
     Route::get('boarding_fee', 'Admin\BoardingFeeController@index')->name('boarding_fee.index');
     Route::get('boarding_fee/{id}/edit', 'Admin\BoardingFeeController@edit')->name('boarding_fee.edit');
+    Route::get('boarding_fee/{id}/installments', 'Admin\BoardingFeeController@createInstallments')->name('boarding_fee.add.installments');
     Route::put('boarding_fee/{id}', 'Admin\BoardingFeeController@update')->name('boarding_fee.update');
     Route::delete('boarding_fee/{id}', 'Admin\BoardingFeeController@destroy')->name('boarding_fee.destroy');
     Route::get('total_boarding_fee/{id}',  'Admin\CollectBoardingFeeController@totalBoardingAmount')->name('getTotalBoardingAmount');
