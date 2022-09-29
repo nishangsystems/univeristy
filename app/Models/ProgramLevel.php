ad534f7fab21a4fbc526f6139592846349f1e099
germanusK <germanuskeming@gmail.com>
2022-09-29 17:06:33 +0100

    29/09/2022 5:06pm

--- app/Models/ProgramLevel.php
+++ app/Models/ProgramLevel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ProgramLevel extends Model
 {
@@ -19,5 +20,15 @@ class ProgramLevel extends Model
     public function level()
     {
         return $this->belongsTo(Level::class);
+    }
+
+    public function campuses()
+    {
+        return $this->hasMany(Campus::class);
+    }
+
+    public function subjects()
+    {
+        return $this->belongsToMany( Subjects::class, ClassSubject::class, 'subject_id', 'class_id');
     }
 }
