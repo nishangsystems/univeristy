871d00f18c0e2869e16de6b5df2edfa830504ab4
germanusK <germanuskeming@gmail.com>
2022-12-03 13:36:28 +0100

    re-commiting work that seems not to have commited during the last attempt

--- resources/views/admin/stock/campus/restore.blade.php
+++ resources/views/admin/stock/campus/restore.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layout')
 @section('section')
 <div class="py-4">
-    <form action="{{route('admin.stock.receive', request('id'))}}" method="post">
+    <form action="{{route('admin.stock.campus.return', [request('campus_id'), request('id')])}}" method="get">
         <div class="row my-2">
             <div class="col-md-3 col-lg-3 text-capitalize">{{__('text.word_quantity')}}</div>
             <div class="col-md-9 col-lg-9">
