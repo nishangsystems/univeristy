1b800274ab011071be236c07ff56e27e25f80682
Akombo Neville Akwo <akomboneville55@gmail.com>
2023-02-26 15:43:18 +0100

    migration to update transactions table

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['student_id', 'amount', 'year_id', 'tel', 'status','payment_purpose','payment_method','reference'];
+
+    protected $table = 'transactions';
 }
