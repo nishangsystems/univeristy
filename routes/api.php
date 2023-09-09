713032c7213449cbea88d04d166bb07c69eb3be3
Crys G <crys@Pro-3s-MacBook-Pro.local>
2023-09-09 14:44:37 +0100

    Modify profile controller in routes

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\ProfileController;
+use App\Http\Controllers\API\Student\ProfileController;
+use App\Http\Controllers\API\ProfileController as UserProfileController;
 
 /*
 |--------------------------------------------------------------------------
