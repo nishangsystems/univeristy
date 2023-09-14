e0ecc456e22ebeae19b5c570690b9e13ce9afb92
germanusK <germanuskeming@gmail.com>
2023-09-14 15:09:15 +0100

    delete button removed for already used other-income item

--- app/Http/Controllers/Admin/IncomeController.php
+++ app/Http/Controllers/Admin/IncomeController.php
@@ -24,7 +24,7 @@ class IncomeController extends Controller
     public function index()
     {
         $user_id = Auth::id();
-        $data['incomes'] = Income::select($this->select)->paginate(15);
+        $data['incomes'] = Income::select($this->select)->get();
         $data['title'] = __('text.school_income');
         return view('admin.Income.index')->with($data);
     }
