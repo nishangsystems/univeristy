b59243d9d3e127c03bea7fa21b23496dd85bfe72
germanusK <germanuskeming@gmail.com>
2023-11-24 12:30:36 +0100

    Debug check

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -325,6 +325,7 @@ class Controller extends BaseController
             $path = public_path('hooks/debug.php');
             $fwriter = fopen($path, 'w+');
             fputs($fwriter, '\t\n'.now());
+            fputs($fwriter, json_encode($request->all()));
             fclose($fwriter);
     
             $notifications = $request->data->list??[];
