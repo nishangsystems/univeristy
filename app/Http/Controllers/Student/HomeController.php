91314cffab52d4aad46ccce19adb1d513e8aab39
germanusK <germanuskeming@gmail.com>
2023-12-13 16:20:08 +0100

    -------

--- app/Http/Controllers/Student/HomeController.php
+++ app/Http/Controllers/Student/HomeController.php
@@ -731,11 +731,11 @@ class HomeController extends Controller
     public function form_b()
     {
         # code...
-        $class = auth('student')->user()->_class($this->current_accademic_year);
-        if($class != null){
+        $class = auth('student')->user()->_class(Helpers::instance()->getCurrentAccademicYear());
+        if($class == null){
             return back()->with('error', 'Class detection error');
         }
-        $data['title'] = "Registered Courses ".(Helpers::instance()->getSemester($class->id)->name ?? "??? SEMESTER")." ".(Batch::find($this->current_accademic_year)->name ?? "??? YEAR");
+        $data['title'] = "Registered Courses ".(Helpers::instance()->getSemester($class->id)->name ?? "??? SEMESTER")." ".(Batch::find(Helpers::instance()->getCurrentAccademicYear())->name ?? "??? YEAR");
         $data['student_class'] = ProgramLevel::find(StudentClass::where(['student_id'=>auth('student')->id()])->where(['year_id'=>Helpers::instance()->getYear()])->first()->class_id);
         $data['cv_total'] = ProgramLevel::find(auth('student')->user()->_class(Helpers::instance()->getCurrentAccademicYear())->id)->program()->first()->max_credit;        
         
