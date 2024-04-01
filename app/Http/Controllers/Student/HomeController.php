3b55dbbb2b01175701546cb47394cd5bf99e4221
nishangsystems <nishangsystems@gmail.com>
2024-04-01 11:47:42 +0100

    res tweek

--- app/Http/Controllers/Student/HomeController.php
+++ app/Http/Controllers/Student/HomeController.php
@@ -263,7 +263,7 @@ class HomeController extends Controller
             $data['grading'] = $class->program()->first()->gradingType->grading()->get() ?? [];
             $res = $data['user']->result()->where('results.batch_id', '=', $year->id)->where('results.semester_id', $semester->id)->distinct()->pluck('subject_id')->toArray();
             $_registered_courses = $data['user']->registered_courses($year->id)->where('semester_id', $semester->id)->pluck('course_id')->toArray();
-            $registered_courses = count($_registered_courses) > 0 ? $_registered_courses : $data['user']->registered_courses($year->id)->whereNull('semester_id')->pluck('course_id')->toArray();
+            $registered_courses = count($_registered_courses) > 0 ? $_registered_courses : $data['user']->registered_courses($year->id)->whereNotNull('resit_id')->pluck('course_id')->toArray();
   
             if(count($registered_courses) == 0){
                 session()->flash('error', __('text.no_courses_registered_phrase'));
