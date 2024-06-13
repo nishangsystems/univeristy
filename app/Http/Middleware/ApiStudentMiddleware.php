dd593a26efcc8b144075a58505c6610ecd5900f0
germanusK <germanuskeming@gmail.com>
2024-06-13 16:47:17 +0100

    -----------------

--- app/Http/Middleware/ApiStudentMiddleware.php
+++ app/Http/Middleware/ApiStudentMiddleware.php
@@ -24,16 +24,16 @@ class ApiStudentMiddleware
 
         $user = auth('student_api')->user();
         if(!($user->parent_phone_number != null && strlen($user->parent_phone_number) > 7)){
-            return response()->json(['message'=>'Complete your profile at '.url().' to continue'], 400);
+            return response()->json(['message'=>'Complete your profile at '.url('/').' to continue'], 400);
         }
         if($user->parent_phone_number == $user->phone ){
-            return response()->json(['message'=>'Update your profile at '.url().' to continue. Parent and student phone number must not be the same.'], 400);
+            return response()->json(['message'=>'Update your profile at '.url('/').' to continue. Parent and student phone number must not be the same.'], 400);
         }
 
         $charge = PlatformCharge::where(['year_id'=>Helpers::instance()->getCurrentAccademicYear()])->first();
         if (Helpers::instance()->payCharges() && ($charge != null && $charge->yearly_amount > 0)){
             if(!(Helpers::instance()->api_has_paid_platform_charges())){
-                return response()->json(['message'=>'Pay PLATFORM CHARGES  at '.url().' to continue.'], 400);
+                return response()->json(['message'=>'Pay PLATFORM CHARGES  at '.url('/').' to continue.'], 400);
             }
         }
 
