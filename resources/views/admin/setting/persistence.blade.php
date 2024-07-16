9e3124673da3e78f7e877ede9af9e01b65358cb1
germanusK <germanuskeming@gmail.com>
2024-07-16 10:48:51 +0100

    persistence for class delegates and HODs added

--- resources/views/admin/setting/persistence.blade.php
+++ resources/views/admin/setting/persistence.blade.php
@@ -9,13 +9,13 @@
                         <div class="col-lg-5 px-3 py-5">
                             <label class="text-secondary text-capitalize">@lang('text.word_operations')</label>
                             <div class="text-uppercase container-fluid text-secondary">
-                                <span class="mx-2 my-1 py-1 border-bottom d-flex"><input value="fee_settings" type="checkbox" class="mx-2" name="operation[]">PERSIST FEE SETTINGS</span>
-                                <span class="mx-2 my-1 py-1 border-bottom d-flex"><input value="course_instructors" type="checkbox" class="mx-2" name="operation[]">PERSIST COURSE INSTRUCTORS</span>
-                                <span class="mx-2 my-1 py-1 border-bottom d-flex"><input value="class_delegates" type="checkbox" class="mx-2" name="operation[]">CLASS DELEGATES</span>
-                                <span class="mx-2 my-1 py-1 border-bottom d-flex"><input value="hods" type="checkbox" class="mx-2" name="operation[]">HODs</span>
+                                <span class="mx-2 my-1 py-1 d-flex"><input value="fee_settings" type="checkbox" class="mx-2" name="operation[]">PERSIST FEE SETTINGS</span>
+                                <span class="mx-2 my-1 py-1 d-flex"><input value="course_instructors" type="checkbox" class="mx-2" name="operation[]">PERSIST COURSE INSTRUCTORS</span>
+                                <span class="mx-2 my-1 py-1 d-flex"><input value="class_delegates" type="checkbox" class="mx-2" name="operation[]">CLASS DELEGATES</span>
+                                <span class="mx-2 my-1 py-1 d-flex"><input value="hods" type="checkbox" class="mx-2" name="operation[]">HODs</span>
                             </div>
                         </div>
-                        <div class="col-lg-7 px-3 py-5 bg-light rounded shadow">
+                        <div class="col-lg-7 px-3 py-5 bg-light shadow">
                             <div class="my-2 row mx-1">
                                 <label class="col-md-4 text-capitalize">@lang('text.base_year')</label>
                                 <select class="col-md-8 form-control rounded" name="base_year" required>
@@ -35,7 +35,7 @@
                                 </select>
                             </div>
                             <div class="py-2 d-flex justify-content-end">
-                                <button class="btn btn-primary btn-sm rounded form-control text-uppercase" type="submit">@lang('text.word_persist')</button>
+                                <button class="btn btn-primary btn-sm rounded form-control text-uppercase" type="submit">@lang('text.copy_data')</button>
                             </div>
                         </div>
                     </div>
