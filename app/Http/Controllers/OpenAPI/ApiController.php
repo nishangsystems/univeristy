339353d608d96e6e11d5636f44b6b44e1d64482b
germanusK <germanuskeming@gmail.com>
2024-07-23 11:37:24 +0100

    -------

--- app/Http/Controllers/OpenAPI/ApiController.php
+++ app/Http/Controllers/OpenAPI/ApiController.php
@@ -182,8 +182,9 @@ class ApiController extends Controller
                     'campus_id'=>$student->campus_id??null, 'admission_batch_id'=>$student->year_id??null,
                     'password'=>Hash::make('12345678'), 'parent_name'=>$student->fee_payer_name??null,
                     'program_id'=>$student->program_first_choice??null, 'region'=>$student->region??null,
-                    'parent_phone_number'=>$student->fee_payer_tel??null, 'division'=>$student->division??null, 
-                    'imported'=>0, 'active'=>1, 'program_status'=>$student->program_status??null
+                    'division'=>$student->division??null, 'imported'=>0, 'active'=>1, 
+                    'program_status'=>$student->program_status??null, 
+                    // 'parent_phone_number'=>$student->fee_payer_tel??null,
                 ];
                 // return ['data'=>$record];
                 $student_instance = new Students($record);
