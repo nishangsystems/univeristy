0fc2d899389535bf909ac6313a7536cb7146f7cb
germanusK <germanuskeming@gmail.com>
2024-09-10 12:25:26 +0100

    searchable selection added

--- resources/views/student_fee_statement.blade.php
+++ resources/views/student_fee_statement.blade.php
@@ -8,32 +8,32 @@
     <style>
         #error_box{
             padding: 2.3rem 1.8rem !important;
-            border: 1px solid rgba(234, 99, 99, 0.9);
-            background-color: rgba(246, 225, 225, 0.9);
-            color: rgba(234, 99, 99, 0.9);
+            border: 1px solid black;
+            background-color: rgb(230, 173, 27);
+            color: white;
         }
         #error_box > strong{
-            color: orangered;
+            color: white;
             margin-right: 1.2rem;
         }
         #message_box{
             padding: 2.3rem 1.8rem !important;
-            border: 1px solid rgba(99, 149, 234, 0.9);
-            background-color: rgba(246, 225, 225, 0.9);
-            color: rgba(91, 160, 217, 0.9);
+            border: 1px solid black;
+            background-color: rgb(230, 173, 27);
+            color: white;
         }
         #message_box > strong{
-            color: rgb(46, 32, 239);
+            color: white;
             margin-right: 1.2rem;
         }
         #success_box{
             padding: 2.3rem 1.8rem !important;
-            border: 1px solid rgba(51, 202, 157, 0.9);
-            background-color: rgba(246, 225, 225, 0.9);
-            color: rgba(92, 239, 193, 0.9);
+            border: 1px solid black;
+            background-color: rgb(230, 173, 27);
+            color: white;
         }
         #success_box > strong{
-            color: rgb(31, 233, 109);
+            color: white;
             margin-right: 1.2rem;
         }
     </style>
